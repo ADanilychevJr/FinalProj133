6e0ebd5706c63efb531562eaee08bde39bc82b8a
ADanilychevJr <danilychev@berkeley.edu>
2015-12-09 14:24:34 -0800

    Added some WDI clean data downloads

--- skeleton.R
+++ skeleton.R
@@ -11,6 +11,10 @@ dir.create("data")
 #Download the raw data
 download.file(foreignAidURL,rawAidLoc)
 download.file(censorshipURL, rawCensorshipLoc)
+#To search stats, WDIsearch("gdp") or WDIsearch("aid"). This is basically a grep
+#Once you find your indicator code, do like this VVV
+gdp_dat = WDI(indicator = "NY.GDP.MKTP.CD", start = 2012, end = 2012)
+gdp_per_capita = WDI(indicator = "NY.GDP.PCAP.CD", start = 2012, end = 2012)
 
 #This calls code/cleanData.R, executing all commands it contains
 source("code/cleanData.R") 
